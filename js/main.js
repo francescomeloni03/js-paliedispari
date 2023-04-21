eac71c4a4b73963e55ccbf026f973277167391e2
Francesco Meloni <francesco.meloni03@icloud.com>
2023-04-21 16:52:30 +0200

    Pari e Dispari

--- js/main.js
+++ js/main.js
@@ -1,36 +1,63 @@
-function palidroma(parola){
+// function palidroma(parola){
 
-    let risultato = false;
+//     let risultato = false;
 
-    console.log(`La parola è ${parola}`)
+//     console.log(`La parola è ${parola}`)
 
 
-    var ParolaDivisa = parola.split('');
-    console.log("La parola divisa:",ParolaDivisa);
+//     var ParolaDivisa = parola.split('');
+//     console.log("La parola divisa:",ParolaDivisa);
 
-    var ParolaDivisa = ParolaDivisa.reverse();
-    console.log("La parola divisa al contrario:",ParolaDivisa);
+//     var ParolaDivisa = ParolaDivisa.reverse();
+//     console.log("La parola divisa al contrario:",ParolaDivisa);
 
-    var ParolaInvertita = ParolaDivisa.join("");
-    console.log("La parola invertita:",ParolaInvertita);
+//     var ParolaInvertita = ParolaDivisa.join("");
+//     console.log("La parola invertita:",ParolaInvertita);
 
-    if(parola == ParolaInvertita){
-        risultato = true;
+//     if(parola == ParolaInvertita){
+//         risultato = true;
        
-    }
+//     }
 
-     return risultato;
+//      return risultato;
+// }
+
+
+// let parola = prompt("inserisci una parola");
+
+
+// if (palidroma(parola)) {
+//     console.log("La parola è palindroma");
+// }
+// else{
+//     console.log("La parola non è palindroma")
+// }
+
+
+
+function PariOrDispari(num1, num2){
+    let a = parseInt(num1);
+    let b = parseInt(num2);
+    let risultato = a + b;
+    console.log("La somma dei numeri è", risultato)
+    let numero = risultato;
+
+    if(numero % 2 == 0) {
+        return "pari";
+    }
+    return "dispari";
 }
 
+let num1 = parseInt( prompt("Inserisci un numero"))
+console.log(`Il primo numero inserito è ${num1}`)
+let num2 = parseInt( prompt("Inserisci un numero"))
+console.log(`Il secondo numero inserito è ${num2}`)
 
-let parola = prompt("inserisci una parola");
-
-
-if (palidroma(parola)) {
-    console.log("La parola è palindroma");
+if (PariOrDispari(num1, num2) == "pari"){
+    console.log("La somma dei numeri è pari");
 }
 else{
-    console.log("La parola non è palindroma")
+    console.log(`la somma dei numeri è dispari`);
 }
 
 
